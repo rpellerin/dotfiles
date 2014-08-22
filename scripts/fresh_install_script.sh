13eac752b90febad37aa3bae0865f676d8ea1205
Romain Pellerin <contact@romainpellerin.eu>
2014-08-23 01:07:36 +0200

    xfce stuff improved

--- scripts/fresh_install_script.sh
+++ scripts/fresh_install_script.sh
@@ -55,21 +55,16 @@ copy_dotfiles() {
     ln -sf $REPO_DIR/.tmux.conf $HOME/ && echo ".tmux.conf copied (symlink)"
     ln -sf -s $REPO_DIR/.zpreztorc $HOME/ && echo ".zpreztorc copied (symlink)"
     mkdir -p $HOME/.config/sublime-text-3/Packages/User && echo "$HOME/.config/sublime-text-3/Packages/User created"
-    ln -sf "$REPO_DIR/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" $HOME/.config/sublime-text-3/Packages/User && {
-        echo "Default (Linux).sublime-keymap copied (symlink)"
-    }
-    ln -sf "$REPO_DIR/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" $HOME/.config/sublime-text-3/Packages/User && {
-        echo "Preferences.sublime-settings copied (symlink)"
+    ln -sf $REPO_DIR/.config/sublime-text-3/Packages/User/* $HOME/.config/sublime-text-3/Packages/User && {
+      echo "-- Some files has been copied in the last created directory (symlinks)"
     }
     mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml && echo "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml created"
-    ln -sf "$REPO_DIR/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/ && {
-      echo "xfce4-keyboard-shortcuts.xml copied (symlink)"
+    ln -sf $REPO_DIR/.config/xfce4/xfconf/xfce-perchannel-xml/* $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/ && {
+      echo "-- Some files has been copied in the last created directory (symlinks)"
     }
-    ln -sf "$REPO_DIR/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/ && {
-      echo "xfce4-panel.xml copied (symlink)"
-    }
-    ln -sf "$REPO_DIR/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/ && {
-      echo "xfce4-power-manager.xml copied (symlink)"
+    mkdir -p $HOME/.config/xfce4/panel && echo "$HOME/.config/xfce4/panel created"
+    ln -sf $REPO_DIR/.config/xfce4/panel/* $HOME/.config/xfce4/panel/ && {
+      echo "-- Some files has been copied in the last created directory (symlinks)"
     }
     ln -sf "$REPO_DIR/.gitconfig" $HOME/ && echo ".gitconfig copied (symlink)"
     cp -i "$REPO_DIR/Images/pause.png" $HOME/Images/pause.png && echo "Images/pause.png copied (true copy)"
