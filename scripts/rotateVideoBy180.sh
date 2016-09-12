02c1dd730bef6947bc9cd397d97f48094c67625c
Romain Pellerin <contact@romainpellerin.eu>
2016-09-12 17:53:52 +0900

    Fix script for rotating videos

--- scripts/rotateVideoBy180.sh
+++ scripts/rotateVideoBy180.sh
@@ -12,8 +12,8 @@ while read -r file
 do
     if [ -z "$file" ]; then exit; fi
     b_name=`basename "$file"`
-    ffmpeg -i "$file" -vf "transpose=2,transpose=2" -c:a copy "$dirr/tmp_$b_name"
-    ffmpeg -i "$dirr/tmp_$b_name" -c copy -metadata:s:v:0 rotate=0 "$dirr/$b_name"
+    ffmpeg -i "$file" -c copy -metadata:s:v:0 rotate=0 "$dirr/tmp_$b_name"
+    ffmpeg -i "$dirr/tmp_$b_name" -vf "transpose=2,transpose=2,format=yuv420p" -codec:v libx264 -c:a copy "$dirr/$b_name"
     rm "$dirr/tmp_$b_name"
     echo "Next."
 done <&0
