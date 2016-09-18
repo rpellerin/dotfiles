edd53499ae1ba3ee284dc545e4440c0aa12f8235
Romain Pellerin <contact@romainpellerin.eu>
2016-09-18 23:29:34 +0900

    Update script video rotate

--- scripts/rotateVideoBy180.sh
+++ scripts/rotateVideoBy180.sh
@@ -12,8 +12,8 @@ while read -r file
 do
     if [ -z "$file" ]; then xdg-open $dirr; exit; fi
     b_name=`basename "$file"`
-    ffmpeg -i "$file" -c copy -metadata:s:v:0 rotate=0 "$dirr/tmp_$b_name"
-    ffmpeg -i "$dirr/tmp_$b_name" -vf "transpose=2,transpose=2,format=yuv420p" -codec:v libx264 -c:a copy "$dirr/$b_name"
+    ffmpeg -i "$file" -c copy -metadata:s:v:0 rotate=0 "$dirr/tmp_$b_name" </dev/null
+    ffmpeg -i "$dirr/tmp_$b_name" -vf "transpose=2,transpose=2,format=yuv420p" -codec:v libx264 -c:a copy "$dirr/$b_name" </dev/null
     rm "$dirr/tmp_$b_name"
     echo "Next."
 done <&0
