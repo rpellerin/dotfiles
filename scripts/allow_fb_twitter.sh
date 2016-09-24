0097370e057e7cf43238e9951098221031a2c836
Romain Pellerin <contact@romainpellerin.eu>
2016-09-24 21:33:43 +0900

    Add scripts

---
+++ scripts/allow_fb_twitter.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sed -i '/twitter/d' /etc/hosts
+sed -i '/facebook/d' /etc/hosts
---
+++ scripts/forbid_fb_twitter.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "127.0.0.1  facebook.com" >> /etc/hosts
+echo "127.0.0.1  www.facebook.com" >> /etc/hosts
+echo "127.0.0.1  twitter.com" >> /etc/hosts
+echo "127.0.0.1  www.twitter.com" >> /etc/hosts
