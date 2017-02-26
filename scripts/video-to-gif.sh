7f5e46cf0fac5811c1daab55adbe13f81fd458f6
Romain Pellerin <contact@romainpellerin.eu>
2017-02-26 15:52:57 -0800

    Improve video script

--- scripts/video-to-gif.sh
+++ scripts/video-to-gif.sh
@@ -2,14 +2,14 @@
 
 # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal usage. First parameter must be the source video and second parameter must be the output GIF"
+if [ "$#" -ne 3 ]; then
+    echo "Illegal usage. First parameter must be the gif width, second must be the source video and third parameter must be the output GIF"
     exit
 fi
 
 palette="/tmp/palette.png"
 
-filters="fps=15,scale=320:-1:flags=lanczos"
+filters="fps=15,scale=$1:-1:flags=lanczos"
 
-ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
-ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+ffmpeg -v warning -i $2 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $2 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $3
