4ee331a45d7a88b8775881b1b4306530380eb1f4
Romain Pellerin <contact@romainpellerin.eu>
2018-01-10 22:11:29 +0100

    Add stuff

--- scripts/set-screensaver.sh
+++ scripts/set-screensaver.sh
@@ -6,10 +6,10 @@
 # https://github.com/iye/lightsOn/blob/master/lightsOn.sh
 # but damn what a pain!
 
-# Set the screensaver to fire after 120 seconds of inactivity
-(sleep 10 && xset s 120) &
+# Set the screensaver to fire after 120 seconds of inactivity, and then every 120seconds another program is run (further below, xset dpms)
+(sleep 10 && xset s 120 120) &
 # The sleep part is required otherwise our setting is overwritten by some other program at startup
 
 
 # Lock the screen using i3lock when screensaver is fired or when lid is closed
-xss-lock /home/romain/git/dotfiles/scripts/lock-screen.sh
+xss-lock -n /home/romain/git/dotfiles/scripts/lock-screen.sh -- xset dpms force off
