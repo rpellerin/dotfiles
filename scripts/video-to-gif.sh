32edfd9ef2fec8ca301f018f8b5d6ef9270f7bb0
Romain Pellerin <romain.pellerin@matters.tech>
2018-02-01 12:57:41 +0100

    Improve video to gif script

--- scripts/video-to-gif.sh
+++ scripts/video-to-gif.sh
@@ -2,14 +2,22 @@
 
 # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
-if [ "$#" -ne 3 ]; then
-    echo "Illegal usage. First parameter must be the gif width, second must be the source video and third parameter must be the output GIF"
+if [ "$#" -lt 2 ]; then
+    echo "Illegal usage. First parameter must be the source video and second parameter must be the output GIF. Third parameter (optional) must be the gif width."
     exit
 fi
 
+size=$3
+
+if [ "$#" -ne 3 ]; then
+    size=$(ffprobe -v quiet -print_format json -show_streams $1 | grep -Po '"width": \K(\d+)')
+fi
+
+echo "Output file GIF will be $size pixel wide"
+
 palette="/tmp/palette.png"
 
-filters="fps=15,scale=$1:-1:flags=lanczos"
+filters="fps=15,scale=$size:-1:flags=lanczos"
 
-ffmpeg -v warning -i $2 -vf "$filters,palettegen" -y $palette
-ffmpeg -v warning -i $2 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $3
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
