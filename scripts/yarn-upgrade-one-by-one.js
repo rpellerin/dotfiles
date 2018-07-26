88be45b9eb87809af77b79e69daa276e4ca07ead
Romain Pellerin <contact@romainpellerin.eu>
2018-07-26 14:19:40 +0200

    Add stuff

--- scripts/yarn-upgrade-one-by-one.js
+++ scripts/yarn-upgrade-one-by-one.js
@@ -4,7 +4,7 @@ const { exec, spawn } = require('child_process')
 const os = require('os')
 const rl = require('readline')
 
-const BLACK_LISTED_PACKAGES = ['rxjs', 'shader', 'jest-junit', 'react-virtualized']
+const BLACK_LISTED_PACKAGES = ['rxjs', 'shader', 'react-virtualized']
 
 const testStrings = [
     'a 1.2.3 1.2.5',
