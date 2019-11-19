14016553da2c3b6c44162b187789e815a3201d68
Romain Pellerin <rpellerin@users.noreply.github.com>
2019-11-19 21:17:38 +0100

    Edit to700px_wide.sh

--- scripts/to700px_wide.sh
+++ scripts/to700px_wide.sh
@@ -6,12 +6,23 @@ echo "Make sure imagemagick is installed."
 
 dirr=`mktemp -d`
 echo "Output files will be put in $dirr"
+echo "Resizing to $size pixel wide..."
 echo "Paste here filepaths, one per line."
+
+size=$1
+
+if [ "$#" -ne 1 ];then
+    size=700
+fi
 
 while read -r file
 do
     if [ -z "$file" ]; then xdg-open $dirr; exit; fi
     b_name=`basename "$file"`
-    convert "$file" -resize 700 "$dirr/$b_name"
+    convert "$file" -resize $size "$dirr/$b_name"
     echo "Next."
 done <&0
+
+exiftool -all= "$dirr/*"
+rm -f "$dirr/*_original"
+echo "Done."
