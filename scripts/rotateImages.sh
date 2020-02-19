e69603e65a7cb2299817fbcf499bc1f8b31b5ae2
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-02-20 00:04:15 +0100

    Minor edits

--- scripts/rotateImages.sh
+++ scripts/rotateImages.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-# Resize an image to 700px width, keeping the ratio
-
 echo "Make sure imagemagick is installed."
-
-dirr=`mktemp -d`
-echo "Output files will be put in $dirr"
-echo "Resizing to $size pixel wide..."
-echo "Paste here filepaths, one per line."
 
 size=$1
 
 if [ "$#" -ne 1 ];then
-    size=700
+    rot=90
 fi
+
+dirr=`mktemp -d`
+echo "Output files will be put in $dirr"
+echo "Rotating by $rot degrees..."
+echo "Paste here filepaths, one per line."
 
 while read -r file
 do
     if [ -z "$file" ]; then xdg-open $dirr; break; fi
     b_name=`basename "$file"`
-    convert "$file" -resize ${size}x "$dirr/$b_name"
+    convert "$file" -rotate ${rot} "$dirr/$b_name"
     echo "Next."
 done <&0
 
