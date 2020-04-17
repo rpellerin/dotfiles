690e9c6b91bec88e7d3ce1bb8f1b147bbd40ef84
Romain Pellerin <contact@romainpellerin.eu>
2020-04-17 15:30:40 +0200

    Minor change

--- scripts/getWeather.py
+++ scripts/getWeather.py
@@ -2,7 +2,7 @@
 
 import urllib.request, json
 
-city = "Paris"
+city = "Berlin"
 api_key = "16f57223a1d6ab7a0cb25f9cbdcd8d56"
 units = "Metric"
 unit_key = "C"
@@ -10,7 +10,7 @@ unit_key = "C"
 weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units={}".format(city, api_key, units)).read())[2:-1])
 
 info = weather["weather"][0]["description"].capitalize()
-temp = int(float(weather["main"]["temp"]))
+temp = str(weather["main"]["temp"])
 
-print("%s, %i °%s" % (info, temp, unit_key))
+print("%s, %s °%s" % (info, temp, unit_key))
 
