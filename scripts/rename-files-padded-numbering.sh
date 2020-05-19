19b6ba8f436ce089a8ceb38af96b3c624023c0cd
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-05-19 23:53:55 +0200

    New script rename-files-padded-numbering

---
+++ scripts/rename-files-padded-numbering.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+dirr=`mktemp -d`
+echo "Output files will be put in $dirr."
+
+echo "The current directory will be processed: $(pwd)"
+echo "Continue? (any key will continue, ^C to exit)"
+read -r continuing
+echo "Processing..."
+# ${f#*.} retrieved the extension from the variable $f
+ls | cat -n | while read n f; do cp -n "$f" "$dirr/$(printf "%05d.${f#*.}" $n)"; done
+xdg-open "$dirr"
+echo "Done"
