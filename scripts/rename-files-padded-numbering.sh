d6d0987e80114f2fb45c8c87fd0410ee801b75a4
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-05-20 22:23:17 +0200

    Minor edits

--- scripts/rename-files-padded-numbering.sh
+++ scripts/rename-files-padded-numbering.sh
@@ -9,8 +9,8 @@ read -r continuing
 echo "Processing..."
 # ${f#*.} retrieved the extension from the variable $f
 ls | cat -n | while read n f; do cp -n "$f" "$dirr/$(printf "%05d.${f#*.}" $n)"; done
-xdg-open "$dirr"
 echo "Done"
 echo "Syncing..."
 sync
 echo "Synced"
+xdg-open "$dirr"
