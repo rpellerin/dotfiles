adc3def44000009393a19a33289581c0a2d1328f
Romain Pellerin <rpellerin@users.noreply.github.com>
2020-09-18 23:31:18 +0200

    Update download-gpx-strava.js

--- scripts/download-gpx-strava.js
+++ scripts/download-gpx-strava.js
@@ -1,13 +1,21 @@
+#!/bin/env node
+
 const { exec } = require("child_process");
 const fetch = require("node-fetch");
 const { exit } = require("process");
+
+if (!process.env.COOKIE) {
+  console.error("No environement variable named COOKIE found.");
+  exit(1);
+}
+
 const run = async () => {
   let ids = [];
   let body;
   let page = 1;
   do {
     const result = await fetch(
-      `https://www.strava.com/athlete/training_activities?keywords=&activity_type=Ride&workout_type=&commute=&private_activities=&trainer=&gear=&search_session_id=REPLACE&new_activity_only=false&order=&page=${page}&per_page=20`,
+      `https://www.strava.com/athlete/training_activities?keywords=&activity_type=Ride&workout_type=&commute=&private_activities=&trainer=&gear=&search_session_id=${process.env.COOKIE}&new_activity_only=false&order=&page=${page}&per_page=20`,
       {
         credentials: "include",
         headers: {
@@ -17,7 +25,7 @@ const run = async () => {
             "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript",
           "Accept-Language": "en-US,en;q=0.5",
           "X-Requested-With": "XMLHttpRequest",
-          Cookie: "REPLACE",
+          Cookie: process.env.COOKIE,
         },
         referrer: "https://www.strava.com/athlete/training",
         method: "GET",
@@ -31,7 +39,7 @@ const run = async () => {
   } while (ids.length < body.total);
   ids.forEach((id, index, array) => {
     exec(
-      `curl 'https://www.strava.com/activities/${id}/export_gpx' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/72.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://www.strava.com/activities/${id}' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Cookie: REPLACE' -H 'Upgrade-Insecure-Requests: 1' -L -o ~/Downloads/gpx-strava/${id}.gpx`,
+      `curl 'https://www.strava.com/activities/${id}/export_gpx' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/72.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: https://www.strava.com/activities/${id}' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Cookie: ${process.env.COOKIE}' -H 'Upgrade-Insecure-Requests: 1' -L -o ~/Downloads/strava-${id}.gpx`,
       (error, stdout, stderr) => {
         console.log(`>>>>>> ${index}/${array.length}`);
         if (error) {
